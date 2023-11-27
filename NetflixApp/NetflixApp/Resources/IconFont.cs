e891d895717beaaad901d7e7263b830fbd2aed91
Abdoulaye Dong <abdoulayedong@gmail.com>
2023-11-27 18:21:52 +0100

    Ajout de fonts et de la SigninPage.

---
+++ NetflixApp/NetflixApp/Resources/IconFont.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetflixApp.Resources
+{
+    public static class IconFont
+    {
+        public const string DotsVertical = "\U000f01d9";
+        public const string CircleMedium = "\U000f09de";
+        public const string ArrowLeft = "\U000f004d";
+    }
+}
